ec6842395c7000341564ef9596f18ef429262f48
Luis Junior Figuereo <lfiguereo@indotel.gob.do>
2020-04-15 22:47:03 -0400

    Sidebar, temas, script

---
+++ src/app/services/services.index.ts
@@ -0,0 +1,4 @@
+
+export { SettingsService } from './setting/settings.service';
+export { SharedService } from './shared/shared.service';
+export { SidebarService } from './shared/sidebar.service';
---
+++ src/app/services/setting/settings.service.ts
@@ -0,0 +1,43 @@
+import { DOCUMENT } from '@angular/common';
+import { Injectable, Inject } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+
+export class SettingsService {
+
+  ajustes: Ajustes = {
+    temaUrl: 'assets/css/colors/default.css',
+    tema: 'default-dark'
+  };
+
+  constructor(@Inject(DOCUMENT) private _document) { }
+
+  guardarAjustes() {
+    localStorage.setItem('ajustes', JSON.stringify(this.ajustes));
+  }
+
+  obtenerAjustes() {
+    if (localStorage.getItem('ajustes')) {
+      this.ajustes = JSON.parse(localStorage.getItem('ajustes'));
+
+      this.aplicarTema(this.ajustes.tema);
+    } else {
+      this.aplicarTema(this.ajustes.tema);
+    }
+  }
+
+  aplicarTema(tema: string) {
+    const url = `assets/css/colors/${tema}.css`;
+    this._document.getElementById('tema').setAttribute('href', url);
+
+    this.ajustes.tema = tema;
+    this.ajustes.temaUrl = url;
+
+    this.guardarAjustes();
+  }
+}
+
+interface Ajustes {
+  temaUrl: string;
+  tema: string;
+}
